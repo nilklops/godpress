13f9efe6a4a95dfbb7fb1436a78c9032eaf63069
peko miyano <peko@hogeclass.com>
2018-11-12 19:46:03 +0900

    Add example server code

--- server.go
+++ server.go
@@ -1,7 +1,39 @@
 package main
 
-import "log"
+import (
+	"github.com/labstack/gommon/log"
+	"github.com/labstack/echo"
+	"time"
+	"net/http"
+	"os"
+	"os/signal"
+	"context"
+)
 
 func main() {
-	log.Println("Godpress")
+	// Setup
+	e := echo.New()
+	e.Logger.SetLevel(log.INFO)
+	e.GET("/", func(c echo.Context) error {
+		time.Sleep(5 * time.Second)
+		return c.JSON(http.StatusOK, "OK")
+	})
+
+	// Start server
+	go func() {
+		if err := e.Start(":1323"); err != nil {
+			e.Logger.Info("shutting down the server")
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 10 seconds.
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
 }
--- server_test.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "testing"
-
-func TestSample(t *testing.T) {
-	main()
-}
